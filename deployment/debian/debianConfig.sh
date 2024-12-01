30fad467ea6baab65fd4aba2e5202b357189962a
Archie Millard <84399154+ArchieMillard@users.noreply.github.com>
2024-12-01 20:01:58 +0000

    Update debianConfig.sh

--- deployment/debian/debianConfig.sh
+++ deployment/debian/debianConfig.sh
@@ -6,7 +6,7 @@ sudo apt install gnome
 # Remove unwanted packages.
 sudo apt purge shotwell gnome-games evolution rhythmbox gnome-software gnome-sound-recorder
 # Install wanted packages.
-sudo apt install bash binutils bison coreutils diffutils findutils gawk gcc grep gzip audacity gimp vlc htop vim gnome-boxes gparted pavucontrol neofetch cmus xmonad xmobar libghc-xmonad-extras-dev ghc yt-dlp links brasero sound-juicer picard git xfonts-terminus fonts-terminus nala qjackctl geary make virt-manager gnome-firmware fwupdate vcdimager arch-install-scripts synaptic software-properties-gtk lshw unattended-upgrades vrms distrobox podman keepassxc dconf-editor gnome-console devede inkscape gnome-tweaks obs-studio v4l2loopback-dkms
+sudo apt install bash binutils bison coreutils diffutils findutils gawk gcc grep gzip audacity gimp vlc htop vim gnome-boxes gparted pavucontrol neofetch cmus xmonad xmobar libghc-xmonad-extras-dev ghc yt-dlp links brasero sound-juicer picard git xfonts-terminus fonts-terminus nala qjackctl geary make virt-manager gnome-firmware fwupdate vcdimager arch-install-scripts synaptic software-properties-gtk lshw unattended-upgrades vrms distrobox podman keepassxc dconf-editor gnome-console devede handbrake inkscape gnome-tweaks obs-studio v4l2loopback-dkms
 # Remove other wanted packages after installation of wanted packages.
 sudo apt purge imagemagick gnome-terminal
 # Configure auto-upgrades.
@@ -30,3 +30,7 @@ exit
 # sudo lvremove /dev/VOLUME_GROUP/LOGICAL_VOLUME
 # To fill all of the unallocated space in the Volume Group:
 # sudo lvextend -l +100%FREE /dev/VOLUME_GROUP/LOGICAL_VOLUME
+# To install libdvdcss
+# git clone https://code.videolan.org/videolan/libdvdcss.git
+# (WHILE IN THE DIRECTORY) ./configure --prefix=/usr
+# sudo make && sudo make install
